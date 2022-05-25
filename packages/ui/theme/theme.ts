056c9e122aa861321f5d2ab70224f89b4fb9fed6
nasser setti <Abdennasser.Essati@gmail.com>
2022-05-25 22:52:38 +0100

    chore: add theme configuration

---
+++ packages/ui/theme/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#1a73e8',
+    tertiary: '#e8e8e8',
+    quaternary: '#f5f5f5',
+    quinary: '#c2c2c2',
+  }
+}
+export type Theme = typeof theme
